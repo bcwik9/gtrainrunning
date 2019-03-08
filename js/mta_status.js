17a9eb4304ba6e22ee1f4eb4f4b787e0d886f3a2
Ben Cwik <bcwik9@gmail.com>
2019-03-08 18:02:01 -0500

    Update mta_status.js

--- js/mta_status.js
+++ js/mta_status.js
@@ -24,7 +24,7 @@ MtaStatus = function(train_line) {
     status_message = "MTA says everything is running smoothly."
     title = "Good Service"
     // need ability to request site data without being on the site
-    any_origin_sites = ['http://cors.io/?', 'https://api.allorigins.ml/get?url=', 'https://allorigins.me/get?url=']
+    any_origin_sites = ['https://cors.io/?', 'https://api.allorigins.ml/get?url=', 'https://allorigins.me/get?url=']
     any_origin_url = any_origin_sites[0] + encodeURIComponent(mta_url) + '&callback=?'
     $.getJSON(any_origin_url, function(data){
         html_data = $(data.contents)
